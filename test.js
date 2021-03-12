63442686f57eb0c72d5b62952ce60a8f658a4aef
AkashBarot080 <78936630+AkashBarot080@users.noreply.github.com>
2021-03-11 21:38:44 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -1,3 +1,71 @@
+$(document).ready(function() {
+	$('input[type="checkbox"]').click(function () {
+ var count=document.querySelectorAll("input:checked").length
+  document.getElementById('no_seat').innerHTML = '';
+	document.getElementById('no_seat').innerHTML = count;
+var e = document.getElementById("m_name").value;
+	
+	if(e=="Spider Man")
+	{
+
+f=count*10;
+document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = f;
+	}
+	else if(e=="Bat Man")
+	{
+f=count*20;
+document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = f;
+
+	}
+	else{
+f=count*15;
+document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = f;
+
+	}	
+
+});
+ 
+});
+
+
+function get_movie(divid) {
+	  $('#' + divid + ' :checkbox:enabled').prop('checked', false);
+
+	var e = document.getElementById("m_name").value;
+	document.getElementById('movie_name').innerHTML = '';
+	document.getElementById('movie_name').innerHTML = e;
+	if(e=="Spider Man")
+	{
+document.getElementById('price').innerHTML = '';
+	document.getElementById('price').innerHTML = "$10";
+  document.getElementById('no_seat').innerHTML = '';
+  document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = 0;
+
+	}
+	else if(e=="Bat Man")
+	{
+document.getElementById('price').innerHTML = '';
+	document.getElementById('price').innerHTML = "$20";
+  document.getElementById('no_seat').innerHTML = '';
+	document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = 0;
+
+	}
+	else{
+		document.getElementById('price').innerHTML = '';
+	document.getElementById('price').innerHTML = "$15";
+  document.getElementById('no_seat').innerHTML = '';
+	document.getElementById('total').innerHTML = '';
+	document.getElementById('total').innerHTML = 0;
+
+	}
+}
+
+
 function myfun()
            {
             
@@ -7,6 +75,10 @@ function myfun()
 	         alert("Invalid, Please enter Card Holder Name");
              return false;
              }
+             if(!isNaN(a)){
+             alert("Invalid, Please enter only alphabets in name");
+             return false;
+             }
 
              var b = document.getElementById("Card Number").value;
                 
@@ -14,10 +86,18 @@ function myfun()
 	         alert("Invalid, Please enter Card Number");
              return false;
              }
-              if(isNaN(b)){
+             if(isNaN(b)){
              alert("Invalid, Please enter only numbers in card number");
              return false;
              }
+             if(b.length<10){
+             alert("Invalid, Please enter 10 digit card number");
+             return false;
+             } 
+             if(b.length>10){
+             alert("Invalid, Please enter 10 digit card number");
+             return false;
+             } 
 
              var c = document.getElementById("CVV").value;
                 
@@ -48,4 +128,3 @@ function myfun()
    
              
            }
-         
